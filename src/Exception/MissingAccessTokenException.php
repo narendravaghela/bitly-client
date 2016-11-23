f28f7dd9f1e2b97e77dd24de9f89c342543c008f
Narendra Vaghela <narendravaghela4389@gmail.com>
2016-11-23 19:28:32 +0530

    Fix exception class

--- src/Exception/MissingAccessTokenException.php
+++ src/Exception/MissingAccessTokenException.php
@@ -2,12 +2,23 @@
 
 namespace Bitly\Exception;
 
-use RuntimeException;
-
 /**
  * MissingAccessTokenException class.  This exception will be thrown from
  * \Bitly\BitlyClient when access token is missing.
  */
-class MissingAccessTokenException extends RuntimeException
+class MissingAccessTokenException extends Exception
 {
+    /**
+     * Constructor
+     *
+     * @param string|null $message If no message is given 'Missing token' will be the message
+     * @param int $code Status code, defaults to 400
+     */
+    public function __construct($message = null, $code = 400)
+    {
+        if (empty($message)) {
+            $message = 'Missing token';
+        }
+        parent::__construct($message, $code);
+    }
 }
